8c2dad81a614c4d9683659f354cc0f921767c960
Trevor Hartman <trevorhartman@gmail.com>
2013-12-04 08:45:01 -0700

    Display static VIR

--- volta.elm
+++ volta.elm
@@ -2,8 +2,12 @@ import Window
 
 wire = outlined (solid grey) (ngon 4 250) |> move (0, 0) |> rotate (degrees 45)
 
-circuit r = [ wire |> rotate (degrees (r / 100)) ]
+circuit r = wire |> rotate (degrees (r / 100))
 
-scene (w,h) circuit = collage w h circuit
+vir = map plainText ["120 V", "15 A", "6 Ω"]
+
+display = flow down vir |> toForm
+
+scene (w,h) circuit = collage w h [circuit,  display]
 
 main = scene <~ Window.dimensions ~ (lift circuit (foldp (+) 0 (fps 40)))
