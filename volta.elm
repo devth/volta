4b7b698a29795287f1f2e46a7522551b0c6b0564
Trevor Hartman <trevorhartman@gmail.com>
2013-12-04 09:35:37 -0700

    Draw a resistor from icons sprite

--- volta.elm
+++ volta.elm
@@ -8,6 +8,10 @@ vir = map plainText ["120 V", "15 A", "6 Ω"]
 
 display = flow down vir |> toForm
 
-scene (w,h) circuit = collage w h [circuit,  display]
+resistor = croppedImage (20,120) 50 40 "icons.svg"
+         |> toForm
+         |> move (100, 100)
+
+scene (w,h) circuit = collage w h [circuit,  display, resistor]
 
 main = scene <~ Window.dimensions ~ (lift circuit (foldp (+) 0 (fps 40)))
