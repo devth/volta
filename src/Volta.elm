01da42472c120ce65116af8f87d266328d01af4b
Trevor Hartman <trevorhartman@gmail.com>
2013-12-20 06:59:45 -0700

    Draw a grid in the background

--- src/Volta.elm
+++ src/Volta.elm
@@ -3,7 +3,9 @@ module Main where
 import Window
 import Sprites
 
-wire = filled yellow (ngon 4 250) |> move (0, 0) |> rotate (degrees 45)
+background w h = tiledImage w h "grid.svg" |> toForm
+
+wire = outlined (solid black) (ngon 4 250) |> move (0, 0) |> rotate (degrees 45)
 
 circuit r = wire |> rotate (degrees (r / 20))
 
@@ -12,6 +14,7 @@ vir = map plainText ["120 V", "15 A", "6 Ω"]
 display = flow down vir |> toForm
                         |> move (-200,200)
 
-scene (w,h) circuit = collage w h [circuit,  display, Sprites.resistor]
+scene (w,h) circuit = collage w h [circuit,  display, Sprites.resistor,
+                                   background w h]
 
 main = scene <~ Window.dimensions ~ (circuit <~ (foldp (+) 0 (fps 40)))
